d48124f8e2452a3738a7eca7cf1d0cfd63414a6b
tryangul <ryan.broughan@gmail.com>
2025-12-08 21:51:52 -0800

    Unstage CDK changes.

--- airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/schema/TableSchemaMapper.kt
+++ airbyte-cdk/bulk/core/load/src/main/kotlin/io/airbyte/cdk/load/schema/TableSchemaMapper.kt
@@ -7,7 +7,6 @@ package io.airbyte.cdk.load.schema
 import io.airbyte.cdk.load.command.DestinationStream
 import io.airbyte.cdk.load.component.ColumnType
 import io.airbyte.cdk.load.data.FieldType
-import io.airbyte.cdk.load.schema.model.StreamTableSchema
 import io.airbyte.cdk.load.schema.model.TableName
 
 /** Transforms input schema elements to destination-specific naming and type conventions. */
@@ -19,8 +18,6 @@ interface TableSchemaMapper {
     fun toColumnName(name: String): String
 
     fun toColumnType(fieldType: FieldType): ColumnType
-
-    fun toFinalSchema(tableSchema: StreamTableSchema) = tableSchema
 
     fun colsConflict(a: String, b: String): Boolean = a.equals(b, ignoreCase = true)
 }
